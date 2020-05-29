2f93849cad626fba7d8f296fbf65f71d4db26a1d
liumin_mbp <liumin2011@gmail.com>
2020-05-29 18:12:47 +0800

    Initial Commit

---
+++ TestNSDictionary/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TestNSDictionary
+//
+//  Created by Bill liu on 2020/5/29.
+//  Copyright © 2020 Mzying. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
